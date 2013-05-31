16d729d392adcce16d0c480605066bcc80538e83
Serge Hallyn <serge.hallyn@canonical.com>
2013-05-31 19:12:12 +0100

    add sysctl to disallow unprivileged CLONE_NEWUSER by default
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>
    [bwh: Remove unneeded binary sysctl bits]
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    [thibaut.sautereau@ssi.gouv.fr: Adapt to sysctl code refactoring]
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -68,6 +68,7 @@
 #include <linux/latencytop.h>
 #include <linux/pid.h>
 #include <linux/delayacct.h>
+#include <linux/user_namespace.h>
 
 #include "../lib/kstrtox.h"
 
@@ -1803,6 +1804,15 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#ifdef CONFIG_USER_NS
+	{
+		.procname	= "unprivileged_userns_clone",
+		.data		= &unprivileged_userns_clone,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+#endif
 #ifdef CONFIG_PROC_SYSCTL
 	{
 		.procname	= "tainted",
