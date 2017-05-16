86c26fb3846c63563f82844bd3d157b5eb473736
Daniel Micay <danielmicay@gmail.com>
2017-05-16 17:51:48 -0400

    usb: add toggle for disabling newly added USB devices
    Based on the public grsecurity patches.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -100,6 +100,9 @@
 #ifdef CONFIG_LOCKUP_DETECTOR
 #include <linux/nmi.h>
 #endif
+#if IS_ENABLED(CONFIG_USB)
+#include <linux/usb.h>
+#endif
 
 #if defined(CONFIG_SYSCTL)
 
@@ -921,6 +924,17 @@ static struct ctl_table kern_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
+#if IS_ENABLED(CONFIG_USB)
+	{
+		.procname	= "deny_new_usb",
+		.data		= &deny_new_usb,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+#endif
 	{
 		.procname	= "ngroups_max",
 		.data		= &ngroups_max,
