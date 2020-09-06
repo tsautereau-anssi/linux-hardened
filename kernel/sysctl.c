6940375de41d173e8b738e9a3bbca76ab529729f
Levente Polyak <levente@leventepolyak.net>
2020-09-06 21:08:16 +0200

    usb: implement dedicated subsystem sysctl tables
    This moves the usb related sysctl knobs to an own usb local sysctl table
    in order to clean up the global sysctl as well as allow the knob to be
    exported and referenced appropriately when building the usb components
    as dedicated modules.
    
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2322,17 +2322,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
-#if IS_ENABLED(CONFIG_USB)
-	{
-		.procname	= "deny_new_usb",
-		.data		= &deny_new_usb,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax_sysadmin,
-		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
-	},
-#endif
 	{
 		.procname	= "ngroups_max",
 		.data		= &ngroups_max,
